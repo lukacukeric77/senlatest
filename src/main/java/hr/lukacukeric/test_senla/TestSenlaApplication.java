240b4e7419c4c6781634ab428ffbc2e392e684b0
lukacukeric77 <lukacukeric77@gmail.com>
2020-11-13 10:39:25 +0100

    Initial commit

---
+++ src/main/java/hr/lukacukeric/test_senla/TestSenlaApplication.java
@@ -0,0 +1,13 @@
+package hr.lukacukeric.test_senla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSenlaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSenlaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/lukacukeric/test_senla/TestSenlaApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.lukacukeric.test_senla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSenlaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
